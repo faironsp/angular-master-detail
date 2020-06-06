b85ace981b367c4a33b34afbd29dfcc894fa4e81
Fabio <faironsp@gmail.com>
2020-06-06 18:07:35 -0300

    Projeto finalizado

--- finansys/src/app/app-routing.module.ts
+++ finansys/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
+  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
+  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
 
-const routes: Routes = [];
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
